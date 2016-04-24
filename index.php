d64eeb446ad6a8637b1d15adf255ee61ba5c2dab
martinfroberg <martin.froberg@gmx.com>
2016-04-24 18:26:14 +0200

    Tog bort foundation ur index
    Grid och koppling till foundation ur index

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ require_once 'functions/init.php' ?>
     <meta charset="utf-8">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <link rel="stylesheet" href="css/stylesheet.css">
-    <link rel="stylesheet" href="css/foundation.css">
     <script type="text/javascript" src="js/modal.js"></script>
     <script type="text/javascript" src="js/threads.js"></script>
 </head>
@@ -16,10 +15,6 @@ require_once 'functions/init.php' ?>
     <?php
     include 'includes/navbar/init.php'; ?>
 
-    <div class="row">
-        <div class="small-12 columns">
-            <?php include 'includes/threads/init.php'; ?>
-        </div>
-    </div>
+    <?php include 'includes/threads/init.php'; ?>
 </body>
 </html>

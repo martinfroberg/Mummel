e1e0e51794a1c8f493a8b2ddedc8174132dff611
merfantapucko <merfantapucko123@hotmail.se>
2016-05-05 17:14:35 +0200

    link in index

--- index.php
+++ index.php
@@ -8,10 +8,19 @@ require_once 'functions/init.php' ?>
     <meta charset="utf-8">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <link rel="stylesheet" href="css/stylesheet.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
     <script type="text/javascript" src="js/modal.js"></script>
     <script type="text/javascript" src="js/comments.js"></script>
 </head>
 <body>
+
+
+
+
+
+
+
     <?php
     include 'includes/navbar/init.php'; ?>
 

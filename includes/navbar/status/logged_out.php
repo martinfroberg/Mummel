0a8946237847b90a48c2318c2fe318dcb2863625
Ashuube <adam.roth96@gmail.com>
2016-05-13 02:06:19 +0200

    Navbar logged out
    Fick texten och knapparna att vara tillsammans i Navbaren. La till en ny
    knapp om oss, tanken är att den ska ligga i en dropdown senare. Använde
    mig av open modal där för att jag hade velat ha texten komma upp i en
    ruta som allt annat för att enbart ha en sida.

--- includes/navbar/status/logged_out.php
+++ includes/navbar/status/logged_out.php
@@ -1,8 +1,12 @@
 <div class="navbarlo">
+  <div class="mummel">
+    <h3>Mummel</h3>
+  </div>
 
   <div class="buttons">
     <button id="login" class="open-modal"><?php echo LOGIN; ?></button>
     <button id="register" class="open-modal"><?php echo REGISTER; ?></button>
+    <button id="profil" class="open-modal">Om oss</button>
   </div>
-  
+
 </div>

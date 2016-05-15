afbcd3c5fb2ecb0f9379ba841c987b92641a3c9e
Ashuube <adam.roth96@gmail.com>
2016-05-15 17:04:30 +0200

    Navbar logged in

--- includes/navbar/status/logged_in.php
+++ includes/navbar/status/logged_in.php
@@ -1,7 +1,12 @@
-<?php echo htmlentities($_SESSION['email']); ?>
-<a href="/mummel/logout.php"><?php echo LOGOUT; ?></a>
-<div class="navbarloggedin">
-<p>
-  hjhwjrhewj hjshfkj ahdakshdaskhdjks
-</p>
+<div class="navbarli">
+  <div class="mummel">
+    <h3>Mummel</h3>
+  </div>
+  <div class="loggedinname"><?php echo htmlentities($_SESSION['email']); ?>
+  </div>
+  <div class="buttons">
+    <button><a href="/mummel/logout.php"><?php echo LOGOUT; ?></a></button>
+    <button id="profil" class="open-modal">Profil</button>
+  </div>
+
 </div>
